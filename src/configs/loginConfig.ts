9f944050efba700fe3e926b5f1752995f690721b
Guilherme Wallace <81390266+gwallace-pontes@users.noreply.github.com>
2024-09-10 10:25:18 -0300

    Delete src/configs directory

--- src/configs/loginConfig.ts
+++
@@ -1,6 +0,0 @@
-export const config_login = {
-    url: 'ldap://10.254.1.12',  // IP do servidor AD
-	baseDN: 'OU=Intervip,DC=ivp,DC=net,DC=br',
-    username: 'administrator@ivp.net.br', // Conta de administrador ou de serviço
-    password: '4c*@DCrb23'      // Senha do administrador
-};
