6dfd7eaf516209af792ba15b75e22bc507e9d9f4
Guilherme Wallace <guilherme.wallace13@hotmail.com>
2024-09-18 10:41:33 -0300

    clean

--- api/v3/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetPlans = void 0;
-var Plans = require("./plan");
-var database_1 = require("../database");
-function GetPlans(name) {
-    return Plans.getPlans(database_1.LOCALHOST, name);
-}
-exports.GetPlans = GetPlans;
