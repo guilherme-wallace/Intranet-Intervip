a33f2a4e4aa74e199011bfa70bb2e63754d9874d
Guilherme Wallace <guilherme.wallace13@hotmail.com>
2026-02-26 09:15:58 -0300

    feat: add IXC user synchronization functionality and integrate axios for API requests

--- src/controllers/usuariosConfig.js
+++ src/controllers/usuariosConfig.js
@@ -40,6 +40,7 @@ exports.UsuariosDB = void 0;
 // src/controllers/usuariosConfig.ts
 var database_1 = require("../../api/database");
 var bcrypt = require("bcrypt");
+var axios_1 = require("axios");
 var saltRounds = 10;
 exports.UsuariosDB = {
     buscarPorUsuario: function (usuario) {
@@ -67,6 +68,88 @@ exports.UsuariosDB = {
                                 resolve();
                             });
                         })];
+            }
+        });
+    }); },
+    sincronizarIXC: function (username) { return __awaiter(void 0, void 0, void 0, function () {
+        var user, url, headers, payload, response, ixcUser, idUsuarioIxc_1, idFuncionarioIxc_1, statusIxc_1, idGrupoIxc_1, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, exports.UsuariosDB.buscarPorUsuario(username)];
+                case 1:
+                    user = _a.sent();
+                    if (!user) {
+                        //console.log(`[DEBUG IXC] Falha: Usuário '${username}' não encontrado na tabela usuarios_intranet.`);
+                        throw new Error('Usuário não encontrado na Intranet');
+                    }
+                    //console.log(`[DEBUG IXC] Usuário local encontrado (ID: ${user.id}). Status atual: id_usuario_ixc=${user.id_usuario_ixc}, id_funcionario_ixc=${user.id_funcionario_ixc}`);
+                    if (user.id_usuario_ixc !== null && user.id_funcionario_ixc !== null) {
+                        //console.log(`[DEBUG IXC] Usuário já possui vínculo com o IXC. Sincronização ignorada.`);
+                        return [2 /*return*/, user];
+                    }
+                    url = "".concat(process.env.IXC_API_URL, "/webservice/v1/usuarios");
+                    headers = {
+                        'Authorization': "Basic ".concat(process.env.IXC_API_TOKEN),
+                        'Content-Type': 'application/json',
+                        'ixcsoft': 'listar'
+                    };
+                    payload = {
+                        qtype: "usuarios.email",
+                        query: "".concat(username, "@"),
+                        oper: "L",
+                        page: "1",
+                        rp: "1"
+                    };
+                    _a.label = 2;
+                case 2:
+                    _a.trys.push([2, 7, , 8]);
+                    return [4 /*yield*/, axios_1.default.post(url, payload, { headers: headers })];
+                case 3:
+                    response = _a.sent();
+                    if (!(response.data && response.data.registros && response.data.registros.length > 0)) return [3 /*break*/, 5];
+                    ixcUser = response.data.registros[0];
+                    idUsuarioIxc_1 = ixcUser.id;
+                    idFuncionarioIxc_1 = ixcUser.funcionario;
+                    statusIxc_1 = ixcUser.status;
+                    idGrupoIxc_1 = ixcUser.id_grupo;
+                    //console.log(`[DEBUG IXC] Preparando UPDATE no banco local -> id_usuario_ixc: ${idUsuarioIxc}, id_funcionario_ixc: ${idFuncionarioIxc}, status_ixc: ${statusIxc}`);
+                    return [4 /*yield*/, new Promise(function (resolve, reject) {
+                            // Adicionamos o id_grupo_ixc na query
+                            var QUERY = "UPDATE usuarios_intranet SET id_usuario_ixc = ?, id_funcionario_ixc = ?, status_ixc = ?, id_grupo_ixc = ? WHERE id = ?";
+                            database_1.LOCALHOST.query(QUERY, [idUsuarioIxc_1, idFuncionarioIxc_1, statusIxc_1, idGrupoIxc_1, user.id], function (err, result) {
+                                if (err) {
+                                    //console.error(`[DEBUG IXC] Erro SQL ao tentar atualizar o banco local:`, err);
+                                    reject(err);
+                                }
+                                else {
+                                    //console.log(`[DEBUG IXC] UPDATE realizado com sucesso! Linhas afetadas: ${result.affectedRows}`);
+                                    resolve(true);
+                                }
+                            });
+                        })];
+                case 4:
+                    //console.log(`[DEBUG IXC] Preparando UPDATE no banco local -> id_usuario_ixc: ${idUsuarioIxc}, id_funcionario_ixc: ${idFuncionarioIxc}, status_ixc: ${statusIxc}`);
+                    _a.sent();
+                    user.id_usuario_ixc = idUsuarioIxc_1;
+                    user.id_funcionario_ixc = idFuncionarioIxc_1;
+                    user.status_ixc = statusIxc_1;
+                    user.id_grupo_ixc = idGrupoIxc_1;
+                    return [2 /*return*/, user];
+                case 5: 
+                //console.log(`[DEBUG IXC] Nenhum usuário foi encontrado no IXC com o e-mail começando com '${username}@'.`);
+                return [2 /*return*/, user];
+                case 6: return [3 /*break*/, 8];
+                case 7:
+                    error_1 = _a.sent();
+                    //console.error("[DEBUG IXC] Ocorreu uma exceção ao comunicar com a API do IXC:");
+                    if (error_1.response) {
+                        //console.error("[DEBUG IXC] Data do erro:", JSON.stringify(error.response.data));
+                    }
+                    else {
+                        //console.error("[DEBUG IXC] Mensagem:", error.message);
+                    }
+                    return [2 /*return*/, user];
+                case 8: return [2 /*return*/];
             }
         });
     }); }
--- src/controllers/usuariosConfig.ts
+++ src/controllers/usuariosConfig.ts
@@ -1,6 +1,7 @@
 // src/controllers/usuariosConfig.ts
 import { LOCALHOST } from '../../api/database';
 import * as bcrypt from 'bcrypt';
+import axios from 'axios';
 
 const saltRounds = 10;
 
@@ -33,5 +34,91 @@ export const UsuariosDB = {
                 resolve();
             });
         });
+    },
+
+    sincronizarIXC: async (username: string): Promise<any> => {
+        //console.log(`\n[DEBUG IXC] Iniciando sincronização para o usuário: '${username}'`);
+
+        const user = await UsuariosDB.buscarPorUsuario(username);
+        
+        if (!user) {
+            //console.log(`[DEBUG IXC] Falha: Usuário '${username}' não encontrado na tabela usuarios_intranet.`);
+            throw new Error('Usuário não encontrado na Intranet');
+        }
+
+        //console.log(`[DEBUG IXC] Usuário local encontrado (ID: ${user.id}). Status atual: id_usuario_ixc=${user.id_usuario_ixc}, id_funcionario_ixc=${user.id_funcionario_ixc}`);
+
+        if (user.id_usuario_ixc !== null && user.id_funcionario_ixc !== null) {
+            //console.log(`[DEBUG IXC] Usuário já possui vínculo com o IXC. Sincronização ignorada.`);
+            return user;
+        }
+
+        const url = `${process.env.IXC_API_URL}/webservice/v1/usuarios`;
+        const headers = {
+            'Authorization': `Basic ${process.env.IXC_API_TOKEN}`,
+            'Content-Type': 'application/json',
+            'ixcsoft': 'listar'
+        };
+        const payload = {
+            qtype: "usuarios.email",
+            query: `${username}@`,
+            oper: "L", 
+            page: "1", 
+            rp: "1"
+        };
+
+        //console.log(`[DEBUG IXC] Fazendo requisição POST para: ${url}`);
+        //console.log(`[DEBUG IXC] Payload enviado:`, JSON.stringify(payload));
+
+        try {
+            const response = await axios.post(url, payload, { headers });
+            
+            //console.log(`[DEBUG IXC] Resposta recebida do IXC. Status Code: ${response.status}`);
+            //console.log(`[DEBUG IXC] Total de registros encontrados no IXC: ${response.data?.total || 0}`);
+            
+            if (response.data && response.data.registros && response.data.registros.length > 0) {
+                const ixcUser = response.data.registros[0];
+                //console.log(`[DEBUG IXC] Dados do primeiro registro retornado pelo IXC:`, JSON.stringify(ixcUser));
+
+                const idUsuarioIxc = ixcUser.id;
+                const idFuncionarioIxc = ixcUser.funcionario;
+                const statusIxc = ixcUser.status;
+                const idGrupoIxc = ixcUser.id_grupo;
+
+                //console.log(`[DEBUG IXC] Preparando UPDATE no banco local -> id_usuario_ixc: ${idUsuarioIxc}, id_funcionario_ixc: ${idFuncionarioIxc}, status_ixc: ${statusIxc}`);
+
+                await new Promise((resolve, reject) => {
+                    // Adicionamos o id_grupo_ixc na query
+                    const QUERY = `UPDATE usuarios_intranet SET id_usuario_ixc = ?, id_funcionario_ixc = ?, status_ixc = ?, id_grupo_ixc = ? WHERE id = ?`;
+                    LOCALHOST.query(QUERY, [idUsuarioIxc, idFuncionarioIxc, statusIxc, idGrupoIxc, user.id], (err, result: any) => {
+                        if (err) {
+                            //console.error(`[DEBUG IXC] Erro SQL ao tentar atualizar o banco local:`, err);
+                            reject(err);
+                        } else {
+                            //console.log(`[DEBUG IXC] UPDATE realizado com sucesso! Linhas afetadas: ${result.affectedRows}`);
+                            resolve(true);
+                        }
+                    });
+                });
+
+                user.id_usuario_ixc = idUsuarioIxc;
+                user.id_funcionario_ixc = idFuncionarioIxc;
+                user.status_ixc = statusIxc;
+                user.id_grupo_ixc = idGrupoIxc;
+                
+                return user;
+            } else {
+                //console.log(`[DEBUG IXC] Nenhum usuário foi encontrado no IXC com o e-mail começando com '${username}@'.`);
+                return user;
+            }
+        } catch (error: any) {
+            //console.error("[DEBUG IXC] Ocorreu uma exceção ao comunicar com a API do IXC:");
+            if (error.response) {
+                //console.error("[DEBUG IXC] Data do erro:", JSON.stringify(error.response.data));
+            } else {
+                //console.error("[DEBUG IXC] Mensagem:", error.message);
+            }
+            return user;
+        }
     }
 };
